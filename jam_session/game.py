a4755d48527becb53087f0254c6adf70732de974
Oscar Hafner <ohgm.dev@gmail.com>
2023-02-17 20:42:16 +0100

    JAM-0: Window size in settings

--- jam_session/game.py
+++ jam_session/game.py
@@ -3,9 +3,11 @@ import pygame
 from jam_session.lib.pygame_utils.create_surface import initialize_game_surface
 from jam_session.lib.base.keyboard_event import keyboard_event_loop
 
+from jam_session.settings import WINDOW_HEIGHT, WINDOW_WIDTH
+
 
 def start_game():
-    game_surface, game_clock = initialize_game_surface()
+    game_surface, game_clock = initialize_game_surface(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
     while True:
         game_clock.tick(60)
         keyboard_event_loop()
