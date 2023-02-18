079fcb4babe2e47ca8f40818af39e652cf4d66ab
Oscar Hafner <ohgm.dev@gmail.com>
2023-02-18 01:15:07 +0100

    First example of GameState and Stage classes

--- jam_session/game.py
+++ jam_session/game.py
@@ -12,11 +12,8 @@ class Asset:
 class AnimatedAsset(Asset):
     pass
 
-
-def start_game():
-    game_surface, game_clock = initialize_game_surface(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
-
-    asset_scene_start_game_title = pygame.image.load('./jam_session/resources/sprites/scene_start_game_title.png')
+"""
+ asset_scene_start_game_title = pygame.image.load('./jam_session/resources/sprites/scene_start_game_title.png')
     asset_scene_start_game_new_game = pygame.image.load('./jam_session/resources/sprites/scene_start_game_new_game.png')
 
     asset_position_title_x = (WINDOW_WIDTH - asset_scene_start_game_title.get_width())/2
@@ -32,10 +29,25 @@ def start_game():
 
     game_surface.blit(asset_scene_start_game_title, (asset_position_x, asset_position_y))
 
+    from jam_session.lib.entities import asset
+    asset_example = asset.from_image_resource('./jam_session/resources/sprites/scene_start_game_title.png', y_origin=200)
+    asset_groups = pygame.sprite.Group()
+    
+    asset_groups.add(asset_example)
+
+    while True:
+        keyboard_event_loop()
+        game_surface.fill((0,0,0))
+        
+        asset_groups.update()
+        asset_groups.draw(game_surface)
+
+        game_clock.tick(4)
+        pygame.display.update()
 
 
     while True:
-        """ game_surface.fill(), rect=None, special_flags=0) """
+         game_surface.fill(), rect=None, special_flags=0)
         game_clock.tick(60)
         keyboard_event_loop()
         pygame.display.update()
@@ -44,19 +56,49 @@ def start_game():
 
     asset_animation = [asset_scene_start_game_new_game, None]
     asset_animation_iterator = cycle(asset_animation)
+"""
+
+class GameState:
+    def __init__(self) -> None:
+        self.current_stage = None
+        self.game_surface = None
+        self.game_clock = None
     
-    while True:
-        keyboard_event_loop()
+    def play(self):
+        while True:
+            self.game_surface.fill((0,0,0))
+            if self.current_stage:
+                self.current_stage.run_iteration()
+            pygame.display.update()
+            self.game_clock.tick(4)
 
 
-        next_sprite = next(asset_animation_iterator)
-        game_surface.fill((0,0,0))
-        game_surface.blit(asset_scene_start_game_title, (asset_position_title_x, asset_position_title_y))
-        if next_sprite:
-            game_surface.blit(next_sprite, (asset_position_x, asset_position_y))
+class Stage:
+    def __init__(self, game_state: GameState) -> None:
+        from jam_session.lib.entities import asset
+        self.game_state = game_state
+        self.asset_example = asset.from_image_resource('./jam_session/resources/sprites/scene_start_game_title.png', y_origin=200)
+        self.asset_groups = pygame.sprite.Group()
+        self.asset_groups.add(self.asset_example)
 
-        game_clock.tick(4)
-        pygame.display.update()
+
+    def run_iteration(self):
+        self.asset_groups.update()
+        self.asset_groups.draw(self.game_state.game_surface)
+
+
+class MainMenuStage(Stage):
+    pass
+
+def start_game():
+    game_state = GameState()
+    game_state.game_surface, game_state.game_clock = initialize_game_surface(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+    game_state.current_stage = MainMenuStage(game_state=game_state)
+
+    game_state.play()
+
+
+
 
 if __name__ == "__main__":
     start_game()
