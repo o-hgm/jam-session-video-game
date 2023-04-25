b9b043973bbea87f67e6fef997408ca829109be7
Oscar Hafner <ohgm.dev@gmail.com>
2023-04-25 14:28:14 +0200

    o-hgm: working with dialogs and components, just an approach

--- docs/example3/with_classes.py
+++ docs/example3/with_classes.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame.draw
 import pygame_gui
+import pygame.key
 
 # Initialize pygame
 pygame.init()
@@ -11,15 +12,15 @@ PLAYER_COLOR = (0, 255, 0)
 player_position = (1, 1)
 
 # Define the total width and height
-GAME_WIDTH, GAME_HEIGHT = 800, 600
+GAME_WIDTH, GAME_HEIGHT = 864, 600
 # Define game settings
-WINDOW_DISPLAY_WIDTH, WINDOW_DISPLAY_HEIGHT = GAME_WIDTH, int(GAME_HEIGHT * 2/3)
+WINDOW_DISPLAY_WIDTH, WINDOW_DISPLAY_HEIGHT = GAME_WIDTH, int(GAME_HEIGHT * 2 / 3)
 
 FPS = 60
 BG_COLOR = (50, 50, 50)
 BG_GAME_COLOR = (100, 100, 100)
 # Define grid settings
-GRID_SIZE = 16
+GRID_SIZE = 96
 MAP_WIDTH, MAP_HEIGHT = 50, 50
 
 DRAWABLE_SURFACE_X = int(WINDOW_DISPLAY_WIDTH / GRID_SIZE)
@@ -33,6 +34,7 @@ def generate_map():
         for j in range(10, 20):
             game_map[i][j] = 1
     return game_map
+
 
 class GameEngine:
     def __init__(self, width, height, fps):
@@ -53,6 +55,8 @@ class GameEngine:
             if event.type == pygame.KEYDOWN:
                 key_event = event.key
 
+            if event.type == pygame.KEYDOWN and self.user_interface.text_input.is_enabled:
+                self.user_interface.handle_text_input(event)
             # Pass events to pygame_gui manager
             self.user_interface.manager.process_events(event)
 
@@ -72,44 +76,113 @@ class GameEngine:
 
 class UserInterfaceInteractive:
     def __init__(self, bg_color, game_engine):
+        self.actions = {}
         self.bg_color = bg_color
-        self.manager = pygame_gui.UIManager((game_engine.screen.get_width(), game_engine.screen.get_height()))
-        
+        self.manager = pygame_gui.UIManager(
+            (game_engine.screen.get_width(), game_engine.screen.get_height())
+        )
+
         self.game_engine = game_engine
         game_engine.user_interface = self
 
         self.build_user_interface()
 
-
     def build_user_interface(self):
         self.ui_dialog_panel = pygame_gui.elements.UIPanel(
             relative_rect=pygame.Rect(
-                (0, int(self.game_engine.screen.get_height() * 2/3)), 
-                (self.game_engine.screen.get_width(), int(self.game_engine.screen.get_height() * 1/3))),
+                (0, int(self.game_engine.screen.get_height() * 2 / 3)),
+                (
+                    self.game_engine.screen.get_width(),
+                    int(self.game_engine.screen.get_height() * 1 / 3),
+                ),
+            ),
             starting_layer_height=1,
-            manager=self.manager
+            manager=self.manager,
         )
+        self.ui_dialog_panel.hide()
 
         # Create a text box for the dialog
         self.ui_dialog_text_box = pygame_gui.elements.UITextBox(
             html_text="",
-            relative_rect=pygame.Rect((20, 20), (self.ui_dialog_panel.rect.width - 40, self.ui_dialog_panel.rect.height - 40)),
+            relative_rect=pygame.Rect(
+                (30, 10),
+                (
+                    500, 100
+                ),
+            ),
             manager=self.manager,
             container=self.ui_dialog_panel,
-            anchors={"left": "left", "right": "right", "top": "top", "bottom": "bottom"},
+            anchors={
+                "left": "left",
+                "right": "right",
+                "top": "top",
+                "bottom": "bottom",
+            },
         )
-    
-    def update_dialog(self, text):
-        self.ui_dialog_text_box.set_text(text)
 
+                # Create input box for user text input
+        self.text_input = pygame_gui.elements.UITextEntryLine(
+            relative_rect=pygame.Rect((30, self.ui_dialog_panel.rect.height - 85), (500, 70)),
+            manager=self.manager,
+            container=self.ui_dialog_panel
+        )
+        self.text_input.disable()
+
+    def handle_text_input(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                user_text = self.text_input.get_text()
+                self.text_input.set_text('')
+                self.update_dialog(f"El jugador dijo: {user_text}")
+
+    def enable_action_buttons(self, actions):
+        # Create buttons for actions
+        self.actions = {
+            "dialogue": pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(
+                    (self.ui_dialog_panel.rect.width - 320, 30), (160, 30)
+                ),
+                text="Dialogar",
+                manager=self.manager,
+                container=self.ui_dialog_panel,
+            ),
+            "harass": pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(
+                    (self.ui_dialog_panel.rect.width - 320, 70), (160, 30)
+                ),
+                text="Hostigar",
+                manager=self.manager,
+                container=self.ui_dialog_panel,
+            ),
+            "shout": pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(
+                    (self.ui_dialog_panel.rect.width - 320, 110), (160, 30)
+                ),
+                text="Gritar",
+                manager=self.manager,
+                container=self.ui_dialog_panel,
+            ),
+            "kill": pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(
+                    (self.ui_dialog_panel.rect.width - 320, 150), (160, 30)
+                ),
+                text="Violencia",
+                manager=self.manager,
+                container=self.ui_dialog_panel,
+            ),
+        }
+
+    def update_dialog(self, text):
+        self.ui_dialog_text_box.set_text(" " + text)
 
     def draw(self, screen, scene, player):
         screen.fill(self.bg_color)
         scene.render(screen, player.position)
         player.render(screen, scene)
 
-        self.manager.update(1/self.game_engine.fps)
+        self.manager.update(1 / self.game_engine.fps)
         self.manager.draw_ui(screen)
+
 
 class UserInterface:
     def __init__(self, bg_color):
@@ -119,7 +192,7 @@ class UserInterface:
         screen.fill(self.bg_color)
         scene.render(screen, player.position)
         player.render(screen, scene)
-    
+
 
 class Player:
     def __init__(self, position, color):
@@ -144,13 +217,30 @@ class Player:
 
     def render(self, screen, scene):
         min_x, min_y = scene.visible_range(self.position)
-        pygame.draw.rect(screen, self.color, ((self.position[0] - min_x) * scene.grid_size, (self.position[1] - min_y) * scene.grid_size, scene.grid_size, scene.grid_size))
+        pygame.draw.rect(
+            screen,
+            self.color,
+            (
+                (self.position[0] - min_x) * scene.grid_size,
+                (self.position[1] - min_y) * scene.grid_size,
+                scene.grid_size,
+                scene.grid_size,
+            ),
+        )
 
 
 import pygame
 
+
 class Scene:
-    def __init__(self, map_data, grid_size, drawable_surface_x, drawable_surface_y, user_interface):
+    def __init__(
+        self,
+        map_data,
+        grid_size,
+        drawable_surface_x,
+        drawable_surface_y,
+        user_interface,
+    ):
         self.map_data = map_data
         self.user_interface = user_interface
         self.grid_size = grid_size
@@ -166,7 +256,7 @@ class Scene:
         if y >= 5 and y <= 15:
             if x >= 10 and x <= 20:
                 return (True, "Asset")
-        
+
         return (False, None)
 
     def visible_range(self, player_position):
@@ -181,23 +271,32 @@ class Scene:
         for y in range(min_y, max_y):
             for x in range(min_x, max_x):
                 try:
-                    cell_color = (200, 200, 200) if self.map_data[y][x] == 0 else (100, 100, 100)
+                    cell_color = (
+                        (200, 200, 200) if self.map_data[y][x] == 0 else (100, 100, 100)
+                    )
                 except:
-                    cell_color = (100,0,0)
-                
+                    cell_color = (100, 0, 0)
+
                 pygame.draw.rect(
-                screen,
-                cell_color,
-                (
-                (x - min_x) * self.grid_size,
-                (y - min_y) * self.grid_size,
-                self.grid_size,
-                self.grid_size,
-                ),
+                    screen,
+                    cell_color,
+                    (
+                        (x - min_x) * self.grid_size,
+                        (y - min_y) * self.grid_size,
+                        self.grid_size,
+                        self.grid_size,
+                    ),
                 )
 
     def handle_interaction(self, player, asset):
+        self.user_interface: UserInterfaceInteractive
+        self.user_interface.ui_dialog_panel.show()
         self.user_interface.update_dialog(f"New interaction with {asset}")
+        self.user_interface.enable_action_buttons({})
+        
+    def clear_interaction(self, player):
+        self.user_interface: UserInterfaceInteractive
+        self.user_interface.ui_dialog_panel.hide()
 
     def update(self, player, key_event):
         pass  # No update needed for the scene in this example, but you can add more logic here if needed.
@@ -205,16 +304,22 @@ class Scene:
 
 import pygame
 
+
 class SpritePlayer:
     def __init__(self, position, sprite_sheet_path, sprite_size):
         self.sprite_map = {
-            'direction_down': [],
-            'direction_left': [],
-            'direction_right': [],
-            'direction_up': []
+            "direction_down": [],
+            "direction_left": [],
+            "direction_right": [],
+            "direction_up": [],
         }
-        self.default_layout = ['direction_down', 'direction_left', 'direction_right', 'direction_up']
-        self.current_action = 'direction_down'
+        self.default_layout = [
+            "direction_down",
+            "direction_left",
+            "direction_right",
+            "direction_up",
+        ]
+        self.current_action = "direction_down"
         self.current_frame = 0
         self.position = position
         self.sprite_size = sprite_size
@@ -228,15 +333,17 @@ class SpritePlayer:
 
         for y in range(0, height, sprite_size):
             index = int(y / sprite_size)
-            current_action = self.default_layout[index] if index < len(self.default_layout) else None
-            print("processing (y=%s) %s" % (y, current_action))
+            current_action = (
+                self.default_layout[index] if index < len(self.default_layout) else None
+            )
             if current_action is not None:
                 for x in range(0, width, sprite_size):
-                    sprite = pygame.Surface((sprite_size, sprite_size), pygame.SRCALPHA, 32)
+                    sprite = pygame.Surface(
+                        (sprite_size, sprite_size), pygame.SRCALPHA, 32
+                    )
                     sprite.blit(sprite_sheet, (0, 0), (x, y, sprite_size, sprite_size))
                     self.sprite_map[current_action].append(sprite)
 
-        print(self.sprite_map)
 
     def update(self, scene, key_event):
         if key_event:
@@ -244,53 +351,70 @@ class SpritePlayer:
 
             if key_event == pygame.K_LEFT:
                 new_x -= 1
-                self.current_action = 'direction_left'
+                self.current_action = "direction_left"
                 # Update the current sprite to face left
                 # self.current_sprite = self.sprites[1]
             elif key_event == pygame.K_RIGHT:
                 new_x += 1
-                self.current_action = 'direction_right'
+                self.current_action = "direction_right"
                 # Update the current sprite to face right
-                #self.current_sprite = self.sprites[2]
+                # self.current_sprite = self.sprites[2]
             elif key_event == pygame.K_UP:
                 new_y -= 1
-                self.current_action = 'direction_up'
+                self.current_action = "direction_up"
                 # Update the current sprite to face up
                 # self.current_sprite = self.sprites[3]
             elif key_event == pygame.K_DOWN:
                 new_y += 1
-                self.current_action = 'direction_down'
+                self.current_action = "direction_down"
                 # Update the current sprite to face down
-                #self.current_sprite = self.sprites[0]
+                # self.current_sprite = self.sprites[0]
 
             total_frames_of_current_action = self.sprite_map[self.current_action] or 1
-            self.current_frame = (self.current_frame + 1) % len(total_frames_of_current_action)
+            self.current_frame = (self.current_frame + 1) % len(
+                total_frames_of_current_action
+            )
+            self.current_sprite = self.sprite_map[self.current_action][
+                self.current_frame
+            ]
 
             if scene.is_walkable(new_x, new_y):
                 self.position = (new_x, new_y)
-                self.current_sprite = self.sprite_map[self.current_action][self.current_frame]
 
             is_colliding, target_asset = scene.is_colliding(new_x, new_y)
-            print(is_colliding, target_asset)
             if is_colliding:
                 scene.handle_interaction(self, target_asset)
-
+            else:
+                scene.clear_interaction(self)
 
     def render(self, screen, scene):
         min_x, min_y = scene.visible_range(self.position)
-        screen.blit(self.current_sprite, ((self.position[0] - min_x) * scene.grid_size, (self.position[1] - min_y) * scene.grid_size))
-
+        screen.blit(
+            self.current_sprite,
+            (
+                (self.position[0] - min_x) * scene.grid_size,
+                (self.position[1] - min_y) * scene.grid_size,
+            ),
+        )
 
 
 def main():
     # Create instances of game components
     game_engine = GameEngine(GAME_WIDTH, GAME_HEIGHT, FPS)
     user_interface = UserInterfaceInteractive(BG_COLOR, game_engine)
-    player = SpritePlayer((1, 1), "sprite_sheet_2.png", 64)
-    scene = Scene(generate_map(), GRID_SIZE, DRAWABLE_SURFACE_X, DRAWABLE_SURFACE_Y, user_interface)
+    # El tamaño de sprite afecta a la collision
+    player = SpritePlayer((1, 1), "Sprite-Wei.png", 128)
+    scene = Scene(
+        generate_map(),
+        GRID_SIZE,
+        DRAWABLE_SURFACE_X,
+        DRAWABLE_SURFACE_Y,
+        user_interface,
+    )
 
     # Run the game
     game_engine.main_loop(scene, player, user_interface)
+
 
 if __name__ == "__main__":
     main()
