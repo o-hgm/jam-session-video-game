efd4ff953db041de78f586a600e75aa3296c1c7f
vimanama <125691815+vimanama@users.noreply.github.com>
2023-05-23 20:49:41 +0200

    Add files via upload

---
+++ jam_session/tiled/reboleomesatest128x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="reboleomesatest128x128" tilewidth="128" tileheight="128" tilecount="4" columns="4">
+ <image source="../objetos/reboleomesatest128x128.png" width="600" height="128"/>
+</tileset>
