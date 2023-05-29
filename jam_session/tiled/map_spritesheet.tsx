b3bdf3c33709f0a231b4f9b4a55984a5f269ddee
vimanama <125691815+vimanama@users.noreply.github.com>
2023-05-29 18:33:54 +0200

    Delete map_spritesheet.tsx

--- jam_session/tiled/map_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="map_spritesheet" tilewidth="128" tileheight="128" tilecount="16" columns="4">
- <image source="map_spritesheet.png" width="512" height="512"/>
-</tileset>
